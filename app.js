49e7666636f798d5da5cba4a353b5b1433876bcf
BrentRevelle <revellebrent@icloud.com>
2025-08-18 18:04:22 -0500

    feat: add crash-test route for review

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 app.use("/", mainRouter);
 
 mongoose
